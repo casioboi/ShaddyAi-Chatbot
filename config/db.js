6f112cf98a6e2281e813775249de4f9bd4a8fb7b
Shadrack Anim <140109716+casioboi@users.noreply.github.com>
2025-07-02 14:48:04 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ let cached = global.mongoose || {conn: null, promise: null};
 export default async function connectDB(){
     if(cached.conn) return cached.conn;
     if(!cached.promise){
-        cached.promise = mongoose.connection(process.env.MONGODB_URL).then((mongoose) => mongoose);
+        cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose) => mongoose);
     }
 
     try {
